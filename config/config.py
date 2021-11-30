141112e6b56fb79b36fc28f4ed46ce67a10a25ce
Justin Courtright <justin.courtright@protonmail.com>
2021-11-30 10:12:36 -0600

    init

---
+++ config/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class config:
+    def __init__(self):
+        self.environ = 'test'
+        self.debug = False
+        self.host = None
+        self.threaded = True
+        self.timeout = 20
+        self.secret = "w-(adsfea\xd5\x06\xbb\x8b\xaa\x8f?,[\x9b\x13\x7f\xfa\xb7^\xfe\x98\x9b\xa12"
+        self.port = 80
---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, session, request, render_template, redirect, url_for, g
+import config.config as config
+import datetime
+
+
+# app setup
+app = Flask(__name__)
+# config
+app_configuration = config.config()
+app.config.from_object(app_configuration)
+app.secret_key = app_configuration.secret
+
+
+
+# session timeout
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = datetime.timedelta(minutes=app_configuration.timeout)
+    session.modified = True
+
+
+@app.route('/', methods=['GET'])
+def Home():
+    if request.method == 'GET':
+        return render_template('Home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=app_configuration.debug,
+            threaded=app_configuration.threaded,
+            port=app_configuration.port,
+            host=app_configuration.host)
